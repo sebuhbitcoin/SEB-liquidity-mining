928f4f1fdbc041f33f9b095e8436cdf51382a3b4
sebuhbitcoin <sebuh.honarchian@gmail.com>
2021-05-11 01:38:18 -0700

    Update programs.ts

--- src/runtime/config/programs.ts
+++ src/runtime/config/programs.ts
@@ -9,6 +9,14 @@ const SEB_MAINNET: Token = {
   id: 0
 };
 
+const WRAP_TESTNET: Token = {
+  contract: 'KT1L1xYJit22TmuhDXaeng4AZDhRqZwcacNj',
+  thumbnailUri: 'ipfs://Qma2o69VRZe8aPsuCUN1VRUE5k67vw2mFDXb35uDkqn17o',
+  decimals: 8,
+  symbol: 'WRAP',
+  name: 'WRAP',
+  id: 0
+};
 
 const programs: { [key in Environment]: ProgramConfig[] } = {
   MAINNET: [
