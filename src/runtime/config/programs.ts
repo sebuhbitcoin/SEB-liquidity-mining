fe2549fe075a4e2548b5deea989357df50a82a7f
sebuhbitcoin <sebuh.honarchian@gmail.com>
2021-05-11 01:46:51 -0700

    Update programs.ts

--- src/runtime/config/programs.ts
+++ src/runtime/config/programs.ts
@@ -1,6 +1,6 @@
 import { Environment, ProgramConfig, Token } from './types';
 
-const SEB_MAINNET: Token = {
+const WRAP_MAINNET: Token = {
   contract: 'KT1981tPmXh4KrUQKZpQKb55kREX7QGJcF3E',
   thumbnailUri: 'https://tzstats.com/logos/tz1R664EP6wjcM1RSUVJ7nrJisTpBW9QyJzP.png',
   decimals: 2,
@@ -21,11 +21,11 @@ const WRAP_TESTNET: Token = {
 const programs: { [key in Environment]: ProgramConfig[] } = {
   MAINNET: [
     {
-      reward: SEB_MAINNET,
+      reward: WRAP_MAINNET,
       farmingContract: 'KT1T8TFtJjhChvniW7deGyKL7kkKearqr8uE',
       pool: {
         contract: 'KT1S4WyNiYPoYheihysddNh5hojNFdiVBwZL',
-        base: SEB_MAINNET,
+        base: WRAP_MAINNET,
         quote: 'xtz'
       }
     },
