abf4bde5c4aaa072facec0f9cafd964c2fc13f33
sebuhbitcoin <sebuh.honarchian@gmail.com>
2021-05-15 07:32:52 -0700

    Update FarmingContractInfo.tsx

--- src/features/farming/components/FarmingContractInfo.tsx
+++ src/features/farming/components/FarmingContractInfo.tsx
@@ -17,7 +17,7 @@ export default function FarmingContractInfo({
             target={'_blank'}
             color={'textSecondary'}
             rel={'noreferrer'}
-            href={`https://quipuswap.com/invest/add-liquidity/${program.pool.contract}_${FarmingContractInfoProps.token.id}`}
+            href={`https://quipuswap.com/invest/add-liquidity/${program.pool.contract}${program.id)`}
           >
             {program.pool.contract}
           </Link>
